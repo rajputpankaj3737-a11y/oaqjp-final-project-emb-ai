3385b3ba0d7566b5496f5ecd2ebeeb821ec0e907
rajputpankaj3737-a11y <rajputpankaj3737@gmail.com>
2026-03-24 22:42:32 +0530

    1st

---
+++ final_project/EmotionDetection/emotion_detection.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=input_json)
+    response_json = response.json()
+
+    emotions = response_json['emotionPredictions'][0]['emotion']
+
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+
+    # Find dominant emotion
+    emotion_scores = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
